9be882b249c59455b52404bca003e14b266279d6
solankiyogesh2511 <solankiyogesh2511@gmail.com>
2025-01-09 16:30:46 +0530

    update with latest code

---
+++ tests/test__Scenario2.py
@@ -0,0 +1,51 @@
+import pytest
+from playwright.sync_api import expect
+from helper.logger import LoggerHelper
+
+def test_Drag_And_Drop_Sliders(page):
+    """
+    Test to Drag & Drop Sliders on LambdaTest Playground.
+    Steps:
+    1. Open the https://www.lambdatest.com/selenium-playground page and click "Drag & Drop Sliders".
+    2. Select the slider with the default value of 15 and drag the bar to make it 95, validating whether the range value shows 95.
+    Args:
+    page (playwright.sync_api.Page): The Playwright page object used to interact with the web page.
+    Validations:
+    - The visibility of the "Drag & Drop Sliders" link.
+    - The current value of the slider before and after dragging.
+    - The final value of the slider is 95.
+    """
+    
+    #1) Open the https://www.lambdatest.com/selenium-playground page and click“Drag & Drop Sliders”
+    LoggerHelper.log_info("Test to Drag & Drop Sliders on LambdaTest Playground")
+    page.goto("https://www.lambdatest.com/selenium-playground/")
+    LoggerHelper.log_info("Opened the https://www.lambdatest.com/selenium-playground page")
+    expect(page.get_by_role("link", name="Drag & Drop Sliders")).to_be_visible()
+    LoggerHelper.log_info("Validated the visibility of the Drag & Drop Sliders link")
+    page.get_by_role("link", name="Drag & Drop Sliders").click()
+    LoggerHelper.log_info("Clicked on the Drag & Drop Sliders link")
+    
+    #2) Select the slider “Default value 15” and drag the bar to make it 95 by validating whether the range value shows 95.
+    slider = page.locator("#slider3").get_by_role("slider")
+    LoggerHelper.log_info("Selected the slider 'Default value 15'")
+    current_value = int(slider.input_value())
+    LoggerHelper.log_info(f"Current value of the slider: {current_value}")
+    target_value = 95
+    LoggerHelper.log_info(f"Target value of the slider: {target_value}")
+
+    # Increment the slider until it reaches the target value
+    while current_value < target_value:
+        LoggerHelper.log_info(f"verify if current value of the slider is less than target value")
+        slider.fill(str(current_value + 1))
+        LoggerHelper.log_info(f"Updated the slider value to: {current_value + 1}")
+        current_value = int(slider.input_value())  # Update the current value
+        LoggerHelper.log_info(f"Slider value updated to: {current_value}")
+        print(f"Slider value updated to: {current_value}")
+
+    # Verify the final value
+    page.get_by_text(str(target_value)).click()
+    LoggerHelper.log_info(f"Clicked on the slider value: {target_value}")
+
+    print(f"Slider reached the target value: {target_value}")
+    expect(slider).to_have_value(str(target_value))
+    LoggerHelper.log_info(f"Validated the slider value: {target_value}")
---
+++ tests/test__Scenario3.py
@@ -0,0 +1,66 @@
+import pytest
+from playwright.sync_api import expect
+from helper.logger import LoggerHelper
+    
+def test_Input_Form_Submit(page):
+    """
+    Test to validate Input Form Submit on LambdaTest Playground.
+    Steps:
+    1. Open the https://www.lambdatest.com/selenium-playground page and click "Input Form Submit".
+    2. Click "Submit" without filling in any information in the form.
+    3. Assert "Please fill in the fields" error message.
+    4. Fill in Name, Email, and other fields.
+    5. From the Country drop-down, select "United States" using the text property.
+    6. Fill in all fields and click "Submit".
+    7. Once submitted, validate the success message "Thanks for contacting us, we will get back to you shortly." on the screen.
+    """
+    # Initialize page object
+    #1) Open the https://www.lambdatest.com/selenium-playground page and click“Input Form Submit”
+    LoggerHelper.log_info("Test to validate Input Form Submit on LambdaTest Playground")
+    page.goto("https://www.lambdatest.com/selenium-playground/")
+    LoggerHelper.log_info("Opened the https://www.lambdatest.com/selenium-playground page")
+    page.get_by_role("link", name="Input Form Submit").click()
+    LoggerHelper.log_info("Clicked on the Input Form Submit link")
+    
+    #2) Click“Submit” without filling in any information in the form.
+    page.get_by_role("button", name="Submit").click()
+    LoggerHelper.log_info("Clicked on the Submit button")
+    
+    #3) Assert “Please fill in the fields” error message.
+    expect(page.locator('input#name[required]')).to_have_js_property('validationMessage', 'Please fill out this field.')
+    LoggerHelper.log_info("Validated the error message 'Please fill out this field.'")
+
+    #4) Fill in Name, Email, and other fields.
+    page.get_by_placeholder("Name", exact=True).fill("Rob")
+    LoggerHelper.log_info("Filled in the Name field")
+    page.get_by_placeholder("Email", exact=True).fill("rob@mailinator.com")
+    LoggerHelper.log_info("Filled in the Email field")
+    page.get_by_placeholder("Password").fill("Rob@123")
+    LoggerHelper.log_info("Filled in the Password field")
+    page.get_by_placeholder("Company").fill("Testing")
+    LoggerHelper.log_info("Filled in the Company field")
+    page.get_by_placeholder("Website").fill("www.test.com")
+    LoggerHelper.log_info("Filled in the Website field")
+    
+    #5) From the Country drop-down, select “United States” using the textproperty.
+    page.get_by_role("combobox").select_option("United States")
+    LoggerHelper.log_info("Selected 'United States' from the Country drop-down")
+    page.get_by_placeholder("City").fill("Jersey City")
+    LoggerHelper.log_info("Filled in the City field")
+    page.get_by_placeholder("Address 1").fill("ABC")
+    LoggerHelper.log_info("Filled in the Address 1 field")
+    page.get_by_placeholder("Address 2").fill("DEF")
+    LoggerHelper.log_info("Filled in the Address 2 field")
+    page.get_by_placeholder("State").fill("New Jersey")
+    LoggerHelper.log_info("Filled in the State field")
+    page.get_by_placeholder("Zip code").fill("07302")
+    LoggerHelper.log_info("Filled in the Zip code field")
+    
+    #6) Fill in all fields and click “Submit”
+    page.get_by_role("button", name="Submit").click()
+    LoggerHelper.log_info("Clicked on the Submit button")
+    
+    #7) Once submitted, validate the success message “Thanks for contactingus, we will get back to you shortly." on the screen.
+    page.wait_for_selector('text="Thanks for contacting us, we will get back to you shortly."')
+    expect(page.locator('text="Thanks for contacting us, we will get back to you shortly."')).to_be_visible()
+    LoggerHelper.log_info("Validated the success message 'Thanks for contacting us, we will get back to you shortly.'")  
